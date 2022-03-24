91d6b412b43004c3b118a253b72f4bf283fe7103
biruk <birukanley2022@gmail.com>
2022-03-24 13:58:58 +0300

    adding event object

--- src/home.js
+++ src/home.js
@@ -2,16 +2,19 @@
 
 
 const Home = (props) => {
+ 
+
     return (
       <div className="home">
         
         <h1 className="header"> {props.titlepage.titles}</h1><br></br>
+        
 
         <p className="body"><br></br>{props.titlepage.body}</p><br></br>
 
         <div className="footer">{props.titlepage.footer}</div>
       </div>
     );
-}
+} 
  
 export default Home;
--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,16 @@
-const Navbar = (props) => {
+const Navbar = () => {
+
+     const alerted = (text, events) => {
+       alert(events.bubbles); // this will handle event objects
+     };
     return ( 
         <div className="navbar">
             <h2 className="header">Kibur blogger</h2>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">NewBlog</a>
-                <a href="/">{props.thirdInde}</a>
+                <button onClick={(events)=>alerted("this is argumentative event", events)}>clikMe</button>
+                
                 
             </div>
         </div>
