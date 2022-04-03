8a4beb07c97433db227456e73c889a37fd7e1bff
biruk <birukanley2022@gmail.com>
2022-04-03 11:44:51 +0300

    updating files

--- src/navbar.js
+++ src/navbar.js
@@ -1,20 +1,23 @@
 const Navbar = () => {
 
      const alerted = (text, events) => {
-       alert(events.bubbles); // this will handle event objects
+       alert(events.bubbles); //             this will handle event objects
      };
-    return ( 
-        <div className="navbar">
-            <h2 className="header">Kibur blogger</h2>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">NewBlog</a>
-                <button onClick={(events)=>alerted("this is argumentative event", events)}>clikMe</button>
-                
-                
-            </div>
+    return (
+      <div className="navbar">
+        <h2 className="header">Kibur blogger</h2>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create">NewBlog</a>
+          
+          <button
+            onClick={(events) => alerted("this is argumentative event", events)}
+          >
+            clikMe
+          </button>
         </div>
-     );
+      </div>
+    );
 }
  
 export default Navbar;
